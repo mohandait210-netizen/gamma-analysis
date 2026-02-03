ff307af494795741452cc710795b54c9edbf89ad
mohandait210-netizen <mohandait210@gmail.com>
2026-02-03 21:34:31 +0100

    app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ if uploaded_file is not None:
         top_gex_strikes.append("0000")
 
     copy_text = f"{call_wall}, {put_wall}, {round(zero_gamma,2) if zero_gamma else 'N/A'}, " \
-                f"{top_gex_strikes[0]}, {top_gex_strikes[1]},"
+                f"{top_gex_strikes[0]}, {top_gex_strikes[1]}, {top_gex_strikes[2]},"
 
     st.text_area(
         "Texte copiable",
