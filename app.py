f6149a10ff9af4cdfcde285c9207429a90ff82e9
mohandait210-netizen <mohandait210@gmail.com>
2026-02-09 16:42:23 +0100

    Update app.py

--- app.py
+++ app.py
@@ -161,7 +161,7 @@ if uploaded_file is not None:
 
     def safe_multiply(val):
         try:
-            return round(float(val) * 41.25, 2)
+            return round(float(val) + 63, 2)
         except:
             return val
 
